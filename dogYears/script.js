6fc657a7df7a1a8564280304a41549cf0ddab5a0
Mouse-dev-689 <vaanmouse@gmail.com>
2024-05-23 13:59:29 -0600

    Add files via upload

---
+++ dogYears/script.js
@@ -0,0 +1,17 @@
+//declare a variable name myAge and set your age
+const myAge = 33
+//declare a variable named earlyYears and set the value as 2
+let  earlyYears = 2
+earlyYears = earlyYears * 10.5 
+//take the myAge variable, and subtract 2 from it
+let laterYears = myAge - 2
+//Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years
+laterYears *= 4
+//console.log(earlyYears)
+//console.log(laterYears)
+// Add earlyYears and laterYears together, and store that in a variable named myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears 
+
+var myName = 'Cam'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years. `)
+
